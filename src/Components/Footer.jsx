a93234fd791d02ede15c8163ae39c6d09a68594a
kunal98singh <singh.kunal9800@gmail.com>
2024-06-03 17:58:33 -0400

    Tailwind Landing Page

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='flex flex-col gap-12 md:flex-row md: justify-between'>
+        <ul className='flex gap-6 font-lato text-gray-400'>
+            <li>
+                <a href="#">Facebook</a>
+            </li>
+            <li>
+                <a href="#">Instagram</a>
+            </li>
+            <li>
+                <a href="#">X(Twitter)</a>
+            </li>
+        </ul>
+        <div className='flex gap-2' >
+            <img src='assets/Help-Avatar.svg' alt='help'/>
+            <div>
+                <p className='font-playfair font-thin'>Have any questions?</p>
+                <a className='font-lato font-medium'>Talk to a s specialist</a>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
